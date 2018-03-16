92af7105d43e5263ac9c61c47903aa107ea056cd
Andy Bell <BellAn@si.edu>
2018-03-16 12:17:17 -0400

    changed param to dryrun

--- R/verify.R
+++ R/verify.R
@@ -4,9 +4,9 @@
 #'
 #'
 #' @param df dataframe with scientific names to validate (col should be named scientificname)
-#' @param allowUnmatched boolean to allow unmatched records (default is false)
+#' @param dryrun check for unmatched names. Doesn't join to the dataframe.
 #' @return dataframe with matches joined to the rows
-verify_sciName <- function(df, allowUnmatched=FALSE){
+verify_sciName <- function(df, dryrun=TRUE){
 
   species_list <- df %>% dplyr::select(scientificname) %>%
     dplyr::distinct() %>% dplyr::pull(scientificname)
@@ -16,16 +16,18 @@ verify_sciName <- function(df, allowUnmatched=FALSE){
   worms_rec_df <- worms_rec %>% dplyr::bind_rows() %>%
     dplyr::filter(status=="accepted")
 
-  joined <- dplyr::left_join(ex, worms_rec_df, by="scientificname")
+  joined <- dplyr::left_join(df, worms_rec_df, by="scientificname")
 
   # print to console the unmatched records
   unmatched_records <- joined %>% dplyr::filter(is.na(AphiaID)) %>% dplyr::select(scientificname) %>% dplyr::distinct()
-  if(nrow(unmatched_records)>0 & !allowUnmatched){
-    print("The following scientific names do not have a match in WoRMS...")
+  if(dryrun){
+    #print("The following scientific names do not have a match in WoRMS...")
     print(unmatched_records)
-    stop(paste("Unknown species fix manually and try again:", unmatched_records))
+    #stop(paste("Unknown species fix manually and try again:", unmatched_records))
+  }
+  else{
+    return(joined)
     }
-  return(joined)
 }
 
 
